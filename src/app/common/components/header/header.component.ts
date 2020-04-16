35e970fe036644e5dcedda111261d53456c66708
scoopspoofsv <shivam.pandey@tothenew.com>
2020-04-17 04:12:14 +0530

    added header and dark theme support

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isDarkMode = false;
   constructor() { }
 
   ngOnInit(): void {
+    if (JSON.parse(localStorage.getItem('darkmode'))) {
+      this.toggleTheme();
+    }
+  }
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkmode', JSON.stringify(this.isDarkMode));
+    document.documentElement.style.setProperty('--background', this.isDarkMode ? '#0a0e1d' : '#fff');
+    document.documentElement.style.setProperty('--text-primary', this.isDarkMode ? '#fff' : '#000');
+    document.documentElement.style.setProperty('--background-light', this.isDarkMode ? '#0a122d' : '#fff');
+    document.documentElement.style.setProperty('--text-light', this.isDarkMode ? '#c5c5c5' : '#444');
+    document.documentElement.style.setProperty('--accent', this.isDarkMode ? '#06a4a6' : '#0c7a93');
   }
 
 }
