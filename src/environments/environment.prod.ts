99da88fe99a3c443461daa914b82869ce7b43cdc
scoopspoofsv <shivam.pandey@tothenew.com>
2020-04-20 02:40:39 +0530

    made build changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  data_url: 'https://api.covid19india.org/data.json',
+  contact_url: 'https://api.rootnet.in/covid19-in/contacts',
+  district_data_url: 'https://api.covid19india.org/state_district_wise.json',
+  world_stats_url: 'https://corona.lmao.ninja/v2/all',
+  news_url:
+    'https://newsapi.org/v2/everything?q=COVID&from=2020-03-20&sortBy=publishedAt&apiKey=c59b8c3a9f6b496d9aeae04241b76937&pageSize=100&language=en'
 };
