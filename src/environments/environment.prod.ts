7de149720a1d515a8bafd5b15ca6f60f17fd448e
scoopspoofsv <shivam.pandey@tothenew.com>
2020-04-21 20:01:53 +0530

    fixed world news not showing up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   district_data_url: 'https://api.covid19india.org/state_district_wise.json',
   world_stats_url: 'https://corona.lmao.ninja/v2/all',
   news_url:
-    'https://newsapi.org/v2/everything?q=COVID&from=2020-03-20&sortBy=publishedAt&apiKey=c59b8c3a9f6b496d9aeae04241b76937&pageSize=100&language=en',
+    'https://newsapi.org/v2/everything?q=COVID&from=2020-03-30&sortBy=publishedAt&apiKey=c59b8c3a9f6b496d9aeae04241b76937&pageSize=100&language=en',
     GH_TOKEN : "3886e665ec9d54f08aeaadbf5e343774fdd81e2c"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   district_data_url: 'https://api.covid19india.org/state_district_wise.json',
   world_stats_url: 'https://corona.lmao.ninja/v2/all',
   news_url:
-    'https://newsapi.org/v2/everything?q=COVID&from=2020-03-20&sortBy=publishedAt&apiKey=c59b8c3a9f6b496d9aeae04241b76937&pageSize=100&language=en'
+    'https://newsapi.org/v2/everything?q=COVID&from=2020-03-30&sortBy=publishedAt&apiKey=c59b8c3a9f6b496d9aeae04241b76937&pageSize=100&language=en'
 };
 
 /*
